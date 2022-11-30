5d7fb3f3caaa13a3c39e5942a3114bdfb04d83b9
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-11-30 11:08:04 +0100

    feat(add): implemented add endpoint

--- HelloWorld/M223ErweitertesHelloWorld/Program.cs
+++ HelloWorld/M223ErweitertesHelloWorld/Program.cs
@@ -3,4 +3,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello rld!");
 
+app.MapGet("calculator/add/{term1:int}/{term2:int}", (int term1, int term2) =>
+{
+    return term1 + term2;
+});
+
 app.Run();
