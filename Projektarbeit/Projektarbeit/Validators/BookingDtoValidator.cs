1788e472e9976ee4aac50ba1a017abc71224d940
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-12-07 11:23:11 +0100

    feat(Validators): added validators

---
+++ Projektarbeit/Projektarbeit/Validators/BookingDtoValidator.cs
@@ -0,0 +1,9 @@
+using FluentValidation;
+using Projektarbeit.Models.Dto;
+
+namespace Projektarbeit.Validators;
+
+public class BookingDtoValidator : AbstractValidator<BookingDto>
+{
+    
+}
---
+++ Projektarbeit/Projektarbeit/Validators/UserDtoValidator.cs
@@ -0,0 +1,9 @@
+using FluentValidation;
+using Projektarbeit.Endpoints.AuthenticationEndpoints;
+
+namespace Projektarbeit.Validators;
+
+public class UserDtoValidator : AbstractValidator<UserDto>
+{
+    
+}
