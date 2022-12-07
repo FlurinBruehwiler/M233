0b07b5d825e187a2f260074f97e43929f939049d
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-12-07 14:47:48 +0100

    feat(User): added firstname and lastname

--- Projektarbeit/Projektarbeit/Models/User.cs
+++ Projektarbeit/Projektarbeit/Models/User.cs
@@ -3,7 +3,8 @@ namespace Projektarbeit.Models;
 public class User
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
+    public required string Firstname { get; set; }
+    public required string LastName { get; set; }
     public required string Email { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
